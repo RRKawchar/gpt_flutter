c9b587347c77d01a077ab4f25c29087d2bd9f55b
rrk94 <riyzurrohmankawchar@gmail.com>
2023-05-09 00:10:23 +0600

    2nd commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gpt_flutter/features_box.dart';
 import 'package:gpt_flutter/pallet.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,11 +8,11 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-      title: const Text("Chat gpt"),
-      centerTitle: true,
-      leading: const Icon(Icons.menu),
-    ),
+      appBar: AppBar(
+        title: const Text("Chat gpt"),
+        centerTitle: true,
+        leading: const Icon(Icons.menu),
+      ),
       body: Column(
         children: [
           Stack(
@@ -22,9 +23,8 @@ class HomePage extends StatelessWidget {
                   width: 120,
                   margin: const EdgeInsets.only(top: 4),
                   decoration: const BoxDecoration(
-                    color: Pallete.assistantCircleColor,
-                    shape: BoxShape.circle
-                  ),
+                      color: Pallete.assistantCircleColor,
+                      shape: BoxShape.circle),
                 ),
               ),
               Container(
@@ -32,29 +32,71 @@ class HomePage extends StatelessWidget {
                 decoration: const BoxDecoration(
                     shape: BoxShape.circle,
                     image: DecorationImage(
-                        image: AssetImage('assets/images/virtualAssistant.png')
-                    )
-                ),
+                        image:
+                            AssetImage('assets/images/virtualAssistant.png'))),
               )
             ],
           ),
-           Container(
-             padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-             margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
-               top: 30,
-             ),
-             decoration: BoxDecoration(
-               border: Border.all(
-                 color: Pallete.borderColor
-               )
-             ),
-             child: const Text("Good Morning, What task can I do for you?",style: TextStyle(
-               fontSize: 25,
-               color: Pallete.mainFontColor,
-               fontFamily: 'Cera Pro'
-             ),),
-           )
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
+              top: 30,
+            ),
+            decoration: BoxDecoration(
+                border: Border.all(color: Pallete.borderColor),
+                borderRadius:
+                    BorderRadius.circular(20).copyWith(topLeft: Radius.zero)),
+            child: const Text(
+              "Good Morning, What task can I do for you?",
+              style: TextStyle(
+                  fontSize: 25,
+                  color: Pallete.mainFontColor,
+                  fontFamily: 'Cera Pro'),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            margin: const EdgeInsets.only(top: 10, left: 22),
+            alignment: Alignment.centerLeft,
+            child: const Text(
+              "Here are a few features",
+              style: TextStyle(
+                  fontFamily: 'Cera Pro',
+                  color: Pallete.mainFontColor,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: const [
+                FeaturesBox(
+                  color: Pallete.firstSuggestionBoxColor,
+                  headerText: "Chat GPT",
+                  descriptionText:
+                      'A smarter way to stay organized and informed with ChatGPT',
+                ),
+                FeaturesBox(
+                  color: Pallete.secondSuggestionBoxColor,
+                  headerText: "Dall-E",
+                  descriptionText:
+                  'Get inspired and stay creative with your personal assistant powered by Dall-E',
+                ),
+                FeaturesBox(
+                  color: Pallete.secondSuggestionBoxColor,
+                  headerText: "Smart Voice Assistant",
+                  descriptionText:
+                  'Get the best of both worlds with a voice assistant powered by Dall-E and Chat GPT',
+                ),
+              ],
+            ),
+          )
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Pallete.firstSuggestionBoxColor,
+        onPressed: (){},
+        child: const Icon(Icons.mic),
       ),
     );
   }
